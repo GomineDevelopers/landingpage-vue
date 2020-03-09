2d7fe12ab28b76d8ca572f759a946de2ebc1d895
Chenxuejiao1399 <48814306+Chenxuejiao1399@users.noreply.github.com>
2020-03-09 11:04:51 +0800

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,11 @@
               <img class="hidden-xs-only" src="../assets/top_banner_content.png" />
               <img class="hidden-sm-and-up" src="../assets/top_banner_content_phone.png" />
               <el-row class="top_content_button flex">
-                <img class="hidden-xs-only" src="../assets/top_button1.png" @click="applyRegister" />
+                <img
+                  class="hidden-xs-only"
+                  src="../assets/top_button1.png"
+                  @click="applyRegister"
+                />
                 <img
                   class="hidden-sm-and-up"
                   src="../assets/phone_top_button2.png"
@@ -50,17 +54,20 @@
           </span>
           <el-row>
             <el-row class="banner_list_title">面向混合云环境的分析</el-row>
-            <el-row class="banner_list_content">可通过单一面板管理操作和交付的运维数据透视分析平台</el-row>
+            <el-row class="banner_list_content"
+              >可通过单一面板管理操作和交付的运维数据透视分析平台</el-row
+            >
             <el-row
               class="banner_list_button"
               @click.native="
                 downloadPdf({
-											name: 'V0877_GA_ENT_DS_Aptare_Data_Sheet_EN-1.pdf',
-											msg: '面向混合云环境的分析',
-											code: 'ZLD-001-P'
-										})
+                  name: 'V0877_GA_ENT_DS_Aptare_Data_Sheet_EN-1.pdf',
+                  msg: '面向混合云环境的分析',
+                  code: 'ZLD-001-P'
+                })
               "
-            >下载产品资料 ›</el-row>
+              >下载产品资料 ›</el-row
+            >
           </el-row>
         </el-col>
         <el-col :xs="24" :sm="24" :md="8" class="flex">
@@ -69,17 +76,21 @@
           </span>
           <el-row>
             <el-row class="banner_list_title">APTARE 应用指南</el-row>
-            <el-row class="banner_list_content">了解 APTARE 运维数据透视分析平台如何助力管理复杂的IT 环境，降低整体存储成本和用例。</el-row>
+            <el-row class="banner_list_content"
+              >了解 APTARE 运维数据透视分析平台如何助力管理复杂的IT
+              环境，降低整体存储成本和用例。</el-row
+            >
             <el-row
               class="banner_list_button"
               @click.native="
                 downloadPdf({
-											name: 'V0991 APTARE_Knows_Guide_use case EN.pdf',
-											msg: 'APTARE应用指南',
-											code: 'ZLD-002-P'
-										})
+                  name: 'V0991 APTARE_Knows_Guide_use case EN.pdf',
+                  msg: 'APTARE应用指南',
+                  code: 'ZLD-002-P'
+                })
               "
-            >下载指南 ›</el-row>
+              >下载指南 ›</el-row
+            >
           </el-row>
         </el-col>
         <el-col :xs="24" :sm="24" :md="8" class="flex">
@@ -87,18 +98,23 @@
             <img class="banner_list_icon" src="../assets/banner_content_icon3.png" />
           </span>
           <el-row>
-            <el-row class="banner_list_title">IDC Vendor Profile：APTARE 运维数据透视分析平台</el-row>
-            <el-row class="banner_list_content">了解 APTARE 运维数据透视分析平台如何解决 IT 资源管理的痛点。</el-row>
+            <el-row class="banner_list_title"
+              >IDC Vendor Profile：APTARE 运维数据透视分析平台</el-row
+            >
+            <el-row class="banner_list_content"
+              >了解 APTARE 运维数据透视分析平台如何解决 IT 资源管理的痛点。</el-row
+            >
             <el-row
               class="banner_list_button"
               @click.native="
                 downloadPdf({
-											name: 'idc_aptare_it_analytics_vendor_profile-3.pdf',
-											msg: 'IDC Vendor Profile : APTARE 运维数据透视分析平台',
-											code: 'ZLD-003-P'
-										})
+                  name: 'idc_aptare_it_analytics_vendor_profile-3.pdf',
+                  msg: 'IDC Vendor Profile : APTARE 运维数据透视分析平台',
+                  code: 'ZLD-003-P'
+                })
               "
-            >下载报告 ›</el-row>
+              >下载报告 ›</el-row
+            >
           </el-row>
         </el-col>
       </el-row>
@@ -126,11 +142,12 @@
           </el-row>
           <el-row
             class="watch_button watch_button1 flex flex_align_center"
-            @click.native="showVideo({
-								src: '/videos/Veritas - 592637 - S01 APTARE_v2 CN.mp4',
-								msg: '面向异构IT环境的运维数据透视分析平台',
-								code: 'ZLD-005-V'
-							})
+            @click.native="
+              showVideo({
+                src: '/videos/Veritas - 592637 - S01 APTARE_v2 CN.mp4',
+                msg: '面向异构IT环境的运维数据透视分析平台',
+                code: 'ZLD-005-V'
+              })
             "
           >
             观看视频演示
@@ -142,11 +159,13 @@
           <img
             class="play_img"
             src="../assets/play_icon.png"
-            @click="showVideo ({
-								src: '/videos/Veritas - 592637 - S01 APTARE_v2 CN.mp4',
-								msg: '面向异构IT环境的运维数据透视分析平台',
-								code: 'ZLD-005-V'
-							})"
+            @click="
+              showVideo({
+                src: '/videos/Veritas - 592637 - S01 APTARE_v2 CN.mp4',
+                msg: '面向异构IT环境的运维数据透视分析平台',
+                code: 'ZLD-005-V'
+              })
+            "
           />
         </el-col>
       </el-row>
@@ -169,11 +188,12 @@
               :xs="24"
               class="hidden-sm-and-down"
               @click.native="
-                showVideo ({
-								src: '/videos/Use case video_APTARE Data Center Optimization Lightboard.mp4',
-								msg: '数据中心管理的优化 简化备份审计和报告',
-								code: 'ZLD-006-V'
-							})"
+                showVideo({
+                  src: '/videos/Use case video_APTARE Data Center Optimization Lightboard.mp4',
+                  msg: '数据中心管理的优化 简化备份审计和报告',
+                  code: 'ZLD-006-V'
+                })
+              "
             >
               <img class="content_body2_img1" src="../assets/content_body2_img1.png" />
             </el-col>
@@ -191,8 +211,12 @@
                   可解决本地和云中备份遇到的诸多常见问题，有助于管理员发现有风险的数据，消除“误报”。Backup
                   Manager 有助于管理员查看所有尝试过的备份，并提供有助解决备份问题的洞察，例如：
                   <ul>
-                    <li>未受保护的数据发现：自动识别不在备份策略保护范围内的客户端和数据集，降低未受保护数据曝光的风险；</li>
-                    <li>可疑备份识别：判断备份软件报告的成功备份是否部分成功备份或失败备份，从而消除”误报“风险。</li>
+                    <li>
+                      未受保护的数据发现：自动识别不在备份策略保护范围内的客户端和数据集，降低未受保护数据曝光的风险；
+                    </li>
+                    <li>
+                      可疑备份识别：判断备份软件报告的成功备份是否部分成功备份或失败备份，从而消除”误报“风险。
+                    </li>
                   </ul>
                   <el-row>
                     对于在环境中使用 ServiceNow 的企业而言，只需轻轻单击一次，APTARE Backup Manager
@@ -203,10 +227,12 @@
                     class="watch_button flex flex_align_center"
                     @click.native="
                       showVideo({
-                        src: '/videos/Use case video_APTARE Data Center Optimization Lightboard.mp4',
+                        src:
+                          '/videos/Use case video_APTARE Data Center Optimization Lightboard.mp4',
                         msg: '数据中心管理的优化 简化备份审计和报告',
                         code: 'ZLD-006-V'
-                      })"
+                      })
+                    "
                   >
                     观看视频
                     <img src="../assets/content_body1_icon2.png" />
@@ -221,10 +247,11 @@
               class="hidden-md-and-up"
               @click.native="
                 showVideo({
-								src: '/videos/Use case video_APTARE Data Center Optimization Lightboard.mp4',
-								msg: '数据中心管理的优化 简化备份审计和报告',
-								code: 'ZLD-006-V'
-							})"
+                  src: '/videos/Use case video_APTARE Data Center Optimization Lightboard.mp4',
+                  msg: '数据中心管理的优化 简化备份审计和报告',
+                  code: 'ZLD-006-V'
+                })
+              "
             >
               <img class="content_body2_img1" src="../assets/content_body2_img1.png" />
             </el-col>
@@ -262,7 +289,8 @@
                         src: '/videos/Use case video_APTARE Storage Optimization Lightboard.mp4',
                         msg: '存储资源管理的优化 有效管理数据存储资源，充分利用可用存储',
                         code: 'ZLD-007-V'
-                      })"
+                      })
+                    "
                   >
                     观看视频
                     <img src="../assets/content_body1_icon2.png" />
@@ -276,10 +304,11 @@
               :md="12"
               @click.native="
                 showVideo({
-                      src: '/videos/Use case video_APTARE Storage Optimization Lightboard.mp4',
-                      msg: '存储资源管理的优化 有效管理数据存储资源，充分利用可用存储',
-                      code: 'ZLD-007-V'
-                    })"
+                  src: '/videos/Use case video_APTARE Storage Optimization Lightboard.mp4',
+                  msg: '存储资源管理的优化 有效管理数据存储资源，充分利用可用存储',
+                  code: 'ZLD-007-V'
+                })
+              "
             >
               <img class="content_body2_img1" src="../assets/content_body2_img2.png" />
             </el-col>
@@ -292,10 +321,11 @@
               class="hidden-sm-and-down"
               @click.native="
                 showVideo({
-                        src: '/videos/Use case_APTARE IT Analytics Chargeback for IaaS.mp4',
-                        msg: 'IT 即服务 提升存储、备份和云的需求管理',
-                        code: 'ZLD-008-V'
-                      })"
+                  src: '/videos/Use case_APTARE IT Analytics Chargeback for IaaS.mp4',
+                  msg: 'IT 即服务 提升存储、备份和云的需求管理',
+                  code: 'ZLD-008-V'
+                })
+              "
             >
               <img class="content_body2_img1" src="../assets/content_body2_img3.png" />
             </el-col>
@@ -312,12 +342,12 @@
                   成本分摊有助于 IT
                   以即服务形式提供商业模式进行运营，了解预算支出点，并按容量和性能向最终客户或业务单位收取成本。APTARE
                   IT Analytics 还可用于特定业务领域的数据增长，从而进一步让 IT 与业务目标保持一致。
-                  <span
-                    class="main_title3"
-                  >APTARE IT Analytics 可帮助您：</span>
+                  <span class="main_title3">APTARE IT Analytics 可帮助您：</span>
                   <ul>
                     <li>获得多租户环境的管理级别报告；</li>
-                    <li>查看已消耗的存储和备份及虚拟机资源，从而有效管理资源成本和需求，并且支持成本分摊；</li>
+                    <li>
+                      查看已消耗的存储和备份及虚拟机资源，从而有效管理资源成本和需求，并且支持成本分摊；
+                    </li>
                     <li>
                       将用户提升到适当水平，并根据用户需求优化存储成本和数据保护
                       <br />实现本地或云中业务IT 成本的可视化。
@@ -330,7 +360,8 @@
                         src: '/videos/Use case_APTARE IT Analytics Chargeback for IaaS.mp4',
                         msg: 'IT 即服务 提升存储、备份和云的需求管理',
                         code: 'ZLD-008-V'
-                      })"
+                      })
+                    "
                   >
                     观看视频
                     <img src="../assets/content_body1_icon2.png" />
@@ -345,10 +376,11 @@
               class="hidden-md-and-up"
               @click.native="
                 showVideo({
-                        src: '/videos/Use case_APTARE IT Analytics Chargeback for IaaS.mp4',
-                        msg: 'IT 即服务 提升存储、备份和云的需求管理',
-                        code: 'ZLD-008-V'
-                      })"
+                  src: '/videos/Use case_APTARE IT Analytics Chargeback for IaaS.mp4',
+                  msg: 'IT 即服务 提升存储、备份和云的需求管理',
+                  code: 'ZLD-008-V'
+                })
+              "
             >
               <img class="content_body2_img1" src="../assets/content_body2_img3.png" />
             </el-col>
@@ -378,10 +410,10 @@
                   class="watch_button watch_button1 flex flex_align_center"
                   @click.native="
                     downloadPdf({
-											name: 'V0877_APTARE IT Analytics_打造混合云环境的统一 IT 分析 CN.pdf',
-											msg: 'APTARE IT Analytics 10.3 新增功能',
-											code: 'ZLD-004-P'
-										})
+                      name: 'V0877_APTARE IT Analytics_打造混合云环境的统一 IT 分析 CN.pdf',
+                      msg: 'APTARE IT Analytics 10.3 新增功能',
+                      code: 'ZLD-004-P'
+                    })
                   "
                 >
                   查看所有受支持的环境
@@ -404,9 +436,9 @@
               “我们手动的报告系统太耗时了，而且效率低下。APTARE IT Analytics
               极大简化了这个流程，有助于集中精力关注更具战略性的问题。”
             </el-row>
-            <el-row
-              class="flex flex_justify_center data_manage_body2_row2_title2"
-            >Chris Davidson,Trustmark National Bank 开放式系统管理副总裁兼经理</el-row>
+            <el-row class="flex flex_justify_center data_manage_body2_row2_title2"
+              >Chris Davidson,Trustmark National Bank 开放式系统管理副总裁兼经理</el-row
+            >
             <el-row class="flex flex_justify_center data_manage_body2_row2_logo">
               <img src="../assets/logo3.png" />
             </el-row>
@@ -414,11 +446,12 @@
               <img
                 src="../assets/data_manage_body2_img5.png"
                 @click="
-              toNewPage({
-										link: 'https://www.veritas.com/zh/cn/customer-success',
-                    msg:'更多客户',
-										code: 'ZLD-009-M'
-									})"
+                  toNewPage({
+                    link: 'https://www.veritas.com/zh/cn/customer-success',
+                    msg: '更多客户',
+                    code: 'ZLD-009-M'
+                  })
+                "
               />
             </el-row>
             <el-row class="flex flex_justify_center data_manage_body2_row2_button">
@@ -475,7 +508,11 @@
         class="hidden-sm-and-up phone_footer_bottom flex flex_justify_center flex_align_center"
         v-if="$route.path == '/'"
       >
-        <img class="hidden-sm-and-up" @click="applyRegister" src="../assets/phone_top_button2.png" />
+        <img
+          class="hidden-sm-and-up"
+          @click="applyRegister"
+          src="../assets/phone_top_button2.png"
+        />
         <img
           class="hidden-sm-and-up"
           @click="demonstrationRegister"
@@ -547,6 +584,8 @@
       :lock-scroll="true"
       :show-close="false"
       center
+      @open="stopSrcoll()"
+      @close="openSrcoll()"
       class="flex flex_align_center flex_justify_center register_dialog"
       :visible.sync="registerDialog"
     >
@@ -559,9 +598,9 @@
       ></el-button>
       <el-row>
         <img class="text_left_icon" src="../assets/content_body1_icon1.png" />
-        <span class="metting_title">{{registerDialogTitle}}</span>
+        <span class="metting_title">{{ registerDialogTitle }}</span>
       </el-row>
-      <el-row class="main_font1">{{registerDialogNotice}}</el-row>
+      <el-row class="main_font1">{{ registerDialogNotice }}</el-row>
       <el-row class="form_content">
         <el-form
           :model="registerRuleForm"
@@ -616,7 +655,11 @@
             </el-select>
           </el-form-item>
           <el-form-item label="省份、城市" prop="city">
-            <el-cascader size="large" :options="options" v-model="registerRuleForm.city"></el-cascader>
+            <el-cascader
+              size="large"
+              :options="options"
+              v-model="registerRuleForm.city"
+            ></el-cascader>
           </el-form-item>
           <el-row class="declare_text flex">
             <el-checkbox v-model="agreeChecked"></el-checkbox>&nbsp;
@@ -625,7 +668,8 @@
               <a
                 href="https://www.veritas.com/company/privacy?cname=20Q2-APJ-ZH%2FCN-NBU-Ransomware-PostEvent-Journey&eid=9554&cid=&elqTrackId=49f25dafdbc04e67b1bff63fc3f13072&elq=5d8e52fac74740c79ca2551dfb62648b&elqaid=9554&elqat=1&elqCampaignId=8681"
                 target="_blank"
-              >隐私声明</a>
+                >隐私声明</a
+              >
             </span>
           </el-row>
           <el-form-item class="form_submit_button">
@@ -892,25 +936,16 @@ export default {
         name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
         phone: [{ required: true, validator: checkphone, trigger: "blur" }],
         email: [{ required: true, validator: checkEmail, trigger: "blur" }],
-        department: [
-          { required: true, message: "请输入部门", trigger: "blur" }
-        ],
-        position: [
-          { required: true, message: "请选择职位", trigger: "change" }
-        ],
-        company: [
-          { required: true, message: "请输入公司名称", trigger: "blur" }
-        ],
-        business: [
-          { required: true, message: "请选择行业", trigger: "change" }
-        ],
-        number: [
-          { required: true, message: "请选择员工人数", trigger: "change" }
-        ],
+        department: [{ required: true, message: "请输入部门", trigger: "blur" }],
+        position: [{ required: true, message: "请选择职位", trigger: "change" }],
+        company: [{ required: true, message: "请输入公司名称", trigger: "blur" }],
+        business: [{ required: true, message: "请选择行业", trigger: "change" }],
+        number: [{ required: true, message: "请选择员工人数", trigger: "change" }],
         city: [{ required: true, message: "请选择城市", trigger: "change" }]
       }
     };
   },
+
   mounted() {
     this.$nextTick(() => {
       this.original = getQueryVariable("original");
@@ -918,6 +953,22 @@ export default {
     });
   },
   methods: {
+    //弹框出现时禁止body滚动
+    stopSrcoll() {
+      var mo = function(e) {
+        e.preventDefault();
+      };
+      document.body.style.overflow = "hidden";
+      document.addEventListener("touchmove", mo, false); //禁止页面滑动
+    },
+    //弹框出现时恢复body滚动
+    openSrcoll() {
+      var mo = function(e) {
+        e.preventDefault();
+      };
+      document.body.style.overflow = ""; //出现滚动条
+      document.removeEventListener("touchmove", mo, false);
+    },
     //报名参会
     applyRegister() {
       let loginOn = this.loginOn();
@@ -1042,6 +1093,7 @@ export default {
     },
     // 提交登录
     login(email) {
+      let vm = this;
       let params = {
         email: email,
         password: 123456
@@ -1052,7 +1104,6 @@ export default {
           let data = res;
           this.logining = false;
           this.isToast = false;
-          this.$refs["registerRuleForm"].resetFields();
           let expires_in = parseInt(data.expires_in);
           let expiresDate = new Date().getTime() + expires_in * 1000;
           localStorage.setItem(
@@ -1230,7 +1281,7 @@ export default {
               industry: this.registerRuleForm.business,
               password: "123456",
               stuff_num: this.registerRuleForm.number,
-              original: this.original //数据来源
+              original: this.original ? this.original : "未知来源" //数据来源
             };
             this.$api
               .register(params)
@@ -1270,9 +1321,12 @@ export default {
         }
       });
     },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
     closeRegisterDialog() {
       this.registerDialog = false;
-      this.$refs["registerRuleForm"].resetFields();
+      this.resetForm("registerRuleForm");
     }
   }
 };
